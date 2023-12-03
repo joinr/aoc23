fc56bb8e8507ed3a56acfcb2d79308e0235ddfc6
tom <tom@that-place>
2023-12-03 04:14:16 -0600

    day 3.

---
+++ src/aoc23/day3.clj
@@ -0,0 +1,170 @@
+(ns aoc23.day3
+  (:require [clojure.string :as s]
+            [aoc23.util :as u]))
+
+(def sample
+"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..")
+
+
+;;want to know where things start/stop
+;;want to get a vector of [{:number 467 :span [0 2]} {:number 114 :span [5 7]}]
+
+(def nums (into #{} (seq "1234567890")))
+
+;;ugly but works.
+(defn extract-syms [xs]
+  (let [res (reduce (fn [{:keys [items current iprev] :as acc} [i c]]
+              (let [emit-symbol?  (not (nums c))
+                    emit-number?  (and current
+                                      (or emit-symbol?
+                                          (not= iprev (dec i))))
+                    accnxt (assoc acc :iprev i)]
+                (cond
+                  (and emit-symbol? emit-number?)
+                    (-> accnxt
+                        (update :items conj
+                                (assoc current :end iprev)
+                                {:start i :string (str c) :end i})
+                        (assoc :current nil))
+                  emit-symbol?
+                  (let [itemsnxt (-> (if current
+                                       (conj items
+                                             (assoc current :end iprev))
+                                       items)
+                                     (conj {:start i :string (str c) :end i}))]
+                    (-> accnxt
+                        (assoc :items itemsnxt)
+                        (assoc :current nil)))
+                  emit-number?
+                    ;;new number.
+                    (-> accnxt
+                        (update :items conj (assoc current :end iprev))
+                        (assoc :current {:start i :string (str c)}))
+                  :else ;accumulate number
+                    (assoc accnxt :current (if current
+                                             (update current :string str c)
+                                             {:start i :string (str c)})))))
+                {:items [] :current nil :iprev -1} xs)]
+    (if (res :current)
+      (let [final (res :current)]
+        (conj (res :items) (assoc final :end
+                                  (+ (final :start) (count (final :string))))))
+      (res :items))))
+
+
+(defn parse-line [ln]
+  (->> ln
+       (map-indexed (fn [i x] [i x]))
+       (filter #(not= (second %) \.))))
+
+(defn symbolic [x]
+  (and (string? x)
+       (= (count x) 1)
+       (not (nums (nth x 0)))))
+
+(defn parse-input [txt]
+  (->> txt
+       s/split-lines
+       (map-indexed (fn [idx ln]
+                      (->> ln  parse-line extract-syms (map (fn [m] (assoc m :row idx))))))
+       (apply concat)
+       (map (fn [{:keys [string] :as m}]
+              (-> m
+                  (assoc :data (if (symbolic string) string (parse-long string)))
+                  (dissoc :string))))))
+
+(defn as-2d [rows] ;;row major orientation.
+  (reduce (fn [acc {:keys [start end row data]}]
+            (let [oldrow (get acc row {})
+                  inner (reduce (fn [acc j]
+                                  (assoc acc j data))
+                                oldrow (range start (inc end)))]
+              (assoc acc row inner))) {} rows))
+
+
+
+(defn adjacents [x y grid]
+  (->> (u/neighbors x y)
+       (map (fn [[x y]] (get-in grid [y x])))
+       (filter number?)
+       set))
+
+(defn parts-ctx [rows]
+  (let [symbols (->> rows (filter (fn [{:keys [data]}] (string? data)))
+                     (mapv (fn [{:keys [start row data]}]
+                             [[start row]  data])))] ;;col major
+    {:symbols (into {} symbols) ;;col-major
+     :grid (as-2d rows) ;;row-major
+     }))
+
+(defn find-parts [{:keys [symbols grid] :as ctx}]
+  (for [[[x y] symb] symbols]
+    (adjacents x y grid)))
+
+(defn solve1 [txt]
+  (->> (parse-input txt)
+       parts-ctx
+       find-parts
+       (filter seq)
+       (apply concat)
+       (reduce +)))
+
+
+;;part 2
+(defn find-gears [{:keys [symbols grid] :as ctx}]
+  (->> (for [[[x y] symb] symbols
+             :when (= symb "*")]
+         (let [res (adjacents x y grid)]
+           (when (>= (count res) 2)
+             res)))
+       (filter identity)))
+
+(defn solve2 [txt]
+  (->> (parse-input txt)
+       parts-ctx
+       find-gears
+       (filter seq)
+       (map (fn [xs] (reduce * xs)))
+       (reduce +)))
+
+;;define adjacency
+
+;;symbols are just [x,y].
+;;so we can look for adjacency by looking to see if any value around them is a digit.
+
+;;given 3,1 are there any symbols in the preceding line that intersect its neighborhood?
+
+;;intersection query can be limited to the row above and below the symbol.
+
+;; 467..114..
+;; ...*......
+;; ..35..633.
+
+
+;;if point is in the same row, only check l/r
+;;above -> u,ul,ur
+;;below -> d,dl,dr
+
+
+;;we can creat an implicit grid.
+;;just walk the rows and expand them into a sparse map.
+
+;;then we get trivial adj queries.
+;;we only need 3 rows at a time, rolling window, to answer adjacency queries.
+
+
+
+(defn ajacents [x y nums]
+  ;;l,r,above,below,ul,ur,dl,dr
+)
+
+
--- src/aoc23/util.clj
+++ src/aoc23/util.clj
@@ -2,5 +2,178 @@
   (:require [clojure.java.io :as jio]))
 
 (defn slurp-resource [path]
-  (slurp (jio/resource "day1.txt")))
+  (slurp (jio/resource path)))
 
+(defn enclose [s char]
+  (case char
+    \[ (str \[ s \])
+    \{ (str \{ s \})
+    \( (str \( s \))
+    (str char s char)))
+
+(defn brackets [s]
+  (enclose s \[))
+
+(defn braces [s]
+  (enclose s \{))
+
+(defn read-as-vector [txt]
+  (-> txt
+      brackets
+      clojure.edn/read-string))
+
+(defn memo-1 [f]
+  (let [^java.util.HashMap cache (java.util.HashMap.)]
+    (fn [x]
+      (if-let [res (.get cache x)]
+        res
+        (let [res (f x)
+              _ (.put cache x res)]
+          res)))))
+
+(def ^:dynamic *logging* nil)
+(defn log [x]
+  (when *logging*
+    (println x))
+  x)
+
+
+(definterface I2D
+  (getEntry ^long [^long r ^long c])
+  (setEntry [^long r ^long c ^long v]))
+
+;;redundant but meh.
+(defn get-entry ^long [^I2D gr ^long x ^long y]
+  (.getEntry gr x y))
+
+(defn set-entry [^I2D gr ^long x ^long y ^long v]
+  (.setEntry gr x y v))
+
+;;no bounds checking lol.
+(defrecord grid [^long w ^long h ^longs entries]
+  I2D
+  (getEntry ^long [this ^long x ^long y]
+    (aget entries (+ (* y w) x)))
+  (setEntry ^long [this ^long x ^long y ^long v]
+    (do (aset entries (+ (* y w) x) v)
+        this)))
+
+(defn new-grid
+  (^grid [^long w ^long h xs]
+   (let [entries (if xs (long-array xs)
+                     (long-array (* w h)))]
+     (->grid w h entries)))
+  (^grid [w h] (new-grid w h nil)))
+
+(defn valid-entry ^long [^grid g ^long x ^long y ^long not-found]
+  (if (and (> y -1) (< y (.-h g))
+           (> x -1) (< x (.-w g)))
+    (get-entry g x y)
+    not-found))
+
+
+(defn neighbors [x y]
+  [[(inc x) y] ;;r
+   [(dec x) y] ;;l
+   [x (inc y)] ;;u
+   [x (dec y)] ;;d
+   [(inc x) (inc y)] ;;ur
+   [(dec x) (inc y)] ;;ul
+   [(dec x) (dec y)] ;;dl
+   [(inc x) (dec y)] ;;dr
+   ])
+
+(defn neighbors4 [x y]
+  [[(inc x) y] ;;r
+   [(dec x) y] ;;l
+   [x (inc y)] ;;u
+   [x (dec y)] ;;d
+   ])
+
+(defn idx->xy [w h n]
+  [(rem n w) (quot n h)])
+
+(defn xy->idx [w h x y]
+  (+ x (* y h)))
+
+(defn adjacency [w h & {:keys [neighbors-fn] :or
+                        {neighbors-fn neighbors}}]
+  (->> (for [x (range w)
+             y (range h)]
+         (let [nebs (->> (neighbors-fn x y)
+                         (filterv (fn [[x y]]
+                                    (and (>= x 0) (< x w)
+                                         (>= y 0) (< y h))))
+                         (map (fn [[x y]]
+                                (xy->idx w h x y))))]
+           [(xy->idx w h x y) [x y] nebs]))
+       (reduce (fn [acc [n xy nebs]]
+                 (assoc acc n {:coord xy :neighbors nebs})) {})))
+
+;;minimally inspired by spork.cljgraph
+(defprotocol IFringe
+  (peek-fringe [fr])
+  (pop-fringe  [fr])
+  (push-fringe [fr w nd]))
+
+;;original lame priorityq impl.
+(defrecord minpq [entries]
+  IFringe
+  (peek-fringe [fr]
+    (when-let [v (first entries)]
+      (v 1)))
+  (pop-fringe  [fr]
+    (if-let [k (first entries)]
+      (minpq. (disj entries k))
+      fr))
+  (push-fringe [fr w nd]
+    (minpq. (conj entries [w nd]))))
+
+(defn min-pq [& xs]
+  (minpq. (into (sorted-set-by
+                 (fn [l r]
+                   (let [res (compare (l 0) (r 0))]
+                     (if-not (zero? res) res
+                             (compare (l 1) (r 1)))))) xs)))
+
+;;playing with indexed priority queue to see if we do better.
+(defrecord minidxq [entries known]
+  IFringe
+  (peek-fringe [fr]
+    (when-let [v (first (vals entries))]
+      (first v)))
+  (pop-fringe  [fr]
+    (if-let [kv (first entries)]
+      (let [k  (key kv)
+            xs (val kv)
+            v (first xs)]
+        (minidxq. (if (> (count xs) 1)
+                 (assoc  entries k (disj xs v))
+                 (dissoc entries k))
+               (dissoc known v)))
+        fr))
+  (push-fringe [fr k v]
+    (if-let [oldk (known v)]
+      (let [old     (entries oldk)]
+        (minidxq. (-> (if (> (count old) 1)
+                     (assoc entries oldk (disj old v))
+                     (dissoc entries oldk))
+                   (update k #(conj (or % #{}) v)))
+               (assoc known v k)))
+      (minidxq. (update entries k #(conj (or % #{}) v))
+             (assoc known v k)))))
+
+(defn min-indexed-pq [& xs]
+  (let [groups (group-by first xs)
+        known (reduce (fn [acc [w k]]
+                        (assoc acc k w)) {} xs)]
+    (minidxq. (into (sorted-map) (for [[k v] groups]
+                                [k (set (map second v))]))
+           known)))
+
+
+(defn float= [l r]
+  (let [diff (- l r)]
+    (if (< (Math/abs diff) 10E-6)
+      true
+      false)))
